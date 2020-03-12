f43c0cb63d777567f69e376482b57aeb57b53066
scott2srikanth <SrikanthReddy.com@gmail.com>
2020-03-12 11:31:36 +0530

    Update HelloServlet.java

--- WT_FirstSession/src/com/kmit/HelloServlet.java
+++ WT_FirstSession/src/com/kmit/HelloServlet.java
@@ -1,6 +1,10 @@
 package com.kmit;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,8 +25,21 @@ public class HelloServlet extends HttpServlet {
 		String username=request.getParameter("username");
 		String password=request.getParameter("password");
 		
-		String dbResult = DBConnection.myConnection(username, password);
-		response.getWriter().append("Served at: "+dbResult);
+		String dbdriver=getServletContext().getInitParameter("JDBCDriver");
+		String dburl=getServletContext().getInitParameter("JDBCUrl");
+		String dbuser=getServletContext().getInitParameter("JDBCUser");
+		String dbpass=getServletContext().getInitParameter("JDBCPassword");
+			
+		User u=new User();
+		String dbResult=DBConnection.myLogin(dbdriver,dburl,dbuser,dbpass,username, password,u);
+		
+		
+		PrintWriter out=response.getWriter();
+		out.println("<html><head></head><body>");
+		out.println("<h1>"+dbResult+"</h1");
+		out.print("<p>"+u.getEmail()+"</p");
+		out.print("</body></html>");
+			
 
 	}
 
